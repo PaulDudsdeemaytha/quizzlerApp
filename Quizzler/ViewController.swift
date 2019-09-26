b301c05a8b70d87cbef440f65659e803de3ccc23
Paul Dudsdeemaytha <pdudsdee@sfu.ca>
2019-09-25 18:00:52 -0700

    removed fatal error at the end of the quiz

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -1,31 +1,39 @@
 //
 //  ViewController.swift
 //  Quizzler
-//
-//  Created by Angela Yu on 25/08/2015.
-//  Copyright (c) 2015 London App Brewery. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
     
     //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var questionNumber : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
-    
+    // Loads the first question
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        // Two equal signs is a check, a single one assigns a value
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        checkAnswer()
+        questionNumber += 1
+        nextQuestion()
     }
     
     
@@ -35,12 +43,24 @@ class ViewController: UIViewController {
     
 
     func nextQuestion() {
-        
+        if questionNumber <= 12 {
+        questionLabel.text = allQuestions.list[questionNumber].questionText
+        }
+        else {
+            print("End of Quiz")
+            questionNumber = 0
+        }
     }
     
     
     func checkAnswer() {
-        
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        if correctAnswer == pickedAnswer {
+            print("Correct")
+        }
+        else {
+            print("Wrong")
+        }
     }
     
     
---
+++ Quizzler/question.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+class Question {
+    // Properties
+    let questionText : String
+    let answer : Bool
+    // Event
+    init(text: String, correctAnswer: Bool){
+        questionText = text
+        answer = correctAnswer
+    }
+    
+    
+}
+
